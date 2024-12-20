4374a20c760abfd560e954732f9db6cd1bd332c8
quy.tn <87492075+oFLSUo@users.noreply.github.com>
2024-12-20 18:28:28 +0700

    oke

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
+const backgroundMusic = document.getElementById('background-music');
 
+// Phát nhạc khi người dùng nhấn vào trang
+document.body.addEventListener('click', () => {
+  if (backgroundMusic.paused) {
+    backgroundMusic.play().catch(err => {
+      console.error('Failed to play music:', err);
+    });
+  }
+});
 let firstSlideContainer = 
 document.getElementsByClassName("slide--content")[0];
 
@@ -21,15 +30,15 @@ thirdCanvas.setAttribute("style","display:none");
 let containerToggleOne = setTimeout(function(){
 	firstSlideContainer.setAttribute("style","display:none");
 	secondSlideContainer.setAttribute("style","display:block");
-},2500);
+},2000);
 
 
 let removeFirstSlide = setTimeout(function(){
 	document.getElementsByClassName("first--slide")[0].setAttribute("style","display:none;")
 	secondCanvas.setAttribute("style","display:block");
-},6500);
+},8500);
 
 let removeSecondCanvas = setTimeout(function(){
 	secondCanvas.setAttribute("style","display:none");
 	thirdCanvas.setAttribute("style","display:block")
-},9800)
+},13800)
